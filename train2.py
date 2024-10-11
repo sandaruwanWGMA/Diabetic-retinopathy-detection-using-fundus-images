a83362805a871aee1556abca4825fdb145ccd040
SandaruwanWGMA <sandaruwanwgma@gmail.com>
2024-10-11 19:59:11 +0530

    w

--- train2.py
+++ train2.py
@@ -21,64 +21,63 @@ def setup_device():
 
 
 def main():
-    device = setup_device()
+    print("Hello!!!")
+    # device = setup_device()
 
-    # Initialize the generator and discriminator
-    generator = CustomUNet().to(device)
-    discriminator = CustomResnet().to(device)
+    # # Initialize the generator and discriminator
+    # generator = CustomUNet().to(device)
+    # discriminator = CustomResnet().to(device)
 
-    # Optimizers
-    opt_G = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
-    opt_D = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+    # # Optimizers
+    # opt_G = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+    # opt_D = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
 
-    # Losses
-    criterion = GANLoss(gan_mode="lsgan").to(device)
+    # # Losses
+    # criterion = GANLoss(gan_mode="lsgan").to(device)
 
-    # Learning rate schedulers
-    scheduler_G = get_scheduler(opt_G, {"lr_policy": "step", "lr_decay_iters": 10})
-    scheduler_D = get_scheduler(opt_D, {"lr_policy": "step", "lr_decay_iters": 10})
+    # # Learning rate schedulers
+    # scheduler_G = get_scheduler(opt_G, {"lr_policy": "step", "lr_decay_iters": 10})
+    # scheduler_D = get_scheduler(opt_D, {"lr_policy": "step", "lr_decay_iters": 10})
 
-    # Define the path to the base directory containing both Low-Res and High-Res directories
-    base_dir = "./MRI Dataset"
+    # # Define the path to the base directory containing both Low-Res and High-Res directories
+    # base_dir = "./MRI Dataset"
 
-    # Create the dataset and dataloader
-    mri_dataset = MRIDataset(base_dir)
-    dataloader = DataLoader(mri_dataset, batch_size=1, shuffle=True)
+    # # Create the dataset and dataloader
+    # mri_dataset = MRIDataset(base_dir)
+    # dataloader = DataLoader(mri_dataset, batch_size=1, shuffle=True)
 
-    num_epochs = 50
-    for epoch in range(num_epochs):
-        print(f"EPOCH NUMBER: {epoch}")
-        for i, data in enumerate(dataloader, 0):
-            print(f"Value of i: {i}")
-            high_res_images = data[1].to(device)
-            low_res_images = data[1].to(device)
+    # num_epochs = 50
+    # for epoch in range(num_epochs):
+    #     print(f"EPOCH NUMBER: {epoch}")
+    #     for i, data in enumerate(dataloader, 0):
+    #         print(f"Value of i: {i}")
+    #         high_res_images = data[1].to(device)
+    #         low_res_images = data[1].to(device)
 
-            # Generate fake images from low-res images
-            fake_images = generator(low_res_images)
+    #         # Generate fake images from low-res images
+    #         fake_images = generator(low_res_images)
 
-            # Prepare data for the discriminator
-            real_input = torch.cat((high_res_images, high_res_images), dim=1)
-            fake_input = torch.cat((fake_images.detach(), high_res_images), dim=1)
+    #         # Prepare data for the discriminator
+    #         real_input = torch.cat((high_res_images, high_res_images), dim=1)
+    #         fake_input = torch.cat((fake_images.detach(), high_res_images), dim=1)
 
-            print("shape of real_input: ", real_input.shape)
+    #         # ===================
+    #         # Update discriminator
+    #         # ===================
+    #         discriminator.zero_grad()
+    #         real_pred = discriminator(real_input)
+    #         loss_D_real = criterion(real_pred, True)
 
-            # ===================
-            # Update discriminator
-            # ===================
-            # discriminator.zero_grad()
-            # real_pred = discriminator(real_input)
-            # loss_D_real = criterion(real_pred, True)
+    #         fake_pred = discriminator(fake_input)
+    #         loss_D_fake = criterion(fake_pred, False)
 
-            # fake_pred = discriminator(fake_input)
-            # loss_D_fake = criterion(fake_pred, False)
+    #         loss_D = (loss_D_real + loss_D_fake) / 2
+    #         loss_D.backward()
+    #         opt_D.step()
 
-            # loss_D = (loss_D_real + loss_D_fake) / 2
-            # loss_D.backward()
-            # opt_D.step()
-
-            # =================
-            # Update generator
-            # =================
+    #         # =================
+    #         # Update generator
+    #         # =================
     #         generator.zero_grad()
 
     #         # We calculate the loss based on the generator's fake output.
