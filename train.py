32ad5fcc1a285f4d245ffbdc284343ebc52e58a5
SandaruwanWGMA <sandaruwanwgma@gmail.com>
2024-10-11 21:47:39 +0530

    e

--- train.py
+++ train.py
@@ -163,7 +163,7 @@ def main():
 if __name__ == "__main__":
     try:
         # Set the environment variable
-        os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:50"
+        os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
         main()
     except Exception as e:
         print(f"An error occurred: {str(e)}")
